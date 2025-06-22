1f54b468af622f96bf54728dddda6e65554f86a6
dodgedlol2 <dodgedlol2@gmail.com>
2025-06-22 16:11:54 +0200

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  env: {
+    CUSTOM_KEY: 'my-value',
+  },
+  images: {
+    domains: ['lh3.googleusercontent.com', 'avatars.githubusercontent.com'],
+  },
+}
+
+module.exports = nextConfig
