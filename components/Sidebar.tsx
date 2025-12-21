e5f51a193bd18471328f0d337654474e7cc5bb61
dodgedlol2 <dodgedlol2@gmail.com>
2025-12-21 12:44:25 +0100

    Update Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -180,7 +180,6 @@ const navigation = [
       },
       {
         name: 'Inactive Supply',
-        href: '/network/inactive-supply',
         icon: (
           <svg className="w-3 h-3" fill="currentColor" viewBox="0 0 24 24">
             <path d="M12,2A10,10 0 0,1 22,12A10,10 0 0,1 12,22A10,10 0 0,1 2,12A10,10 0 0,1 12,2M16.2,16.2L11,13V7H12.5V12.2L17,14.9L16.2,16.2Z"/>
