11b4589284c62d08ab6e1bdee9f6a6b7882985d2
dodgedlol2 <dodgedlol2@gmail.com>
2025-12-28 12:23:04 +0100

    Update SmartTopBottomIndicator.tsx

--- components/charts/SmartTopBottomIndicator.tsx
+++ components/charts/SmartTopBottomIndicator.tsx
@@ -3,7 +3,7 @@ import React, { useState, useMemo } from 'react'
 import dynamic from 'next/dynamic'
 import { KaspaMetric } from '@/lib/sheets'
 
-// Dynamically import Plotly to avoid SSR issuesss
+// Dynamically import Plotly to avoid SSR issues
 const Plot = dynamic(() => import('react-plotly.js'), { ssr: false })
 
 export interface InactiveSupplyDataPoint {
